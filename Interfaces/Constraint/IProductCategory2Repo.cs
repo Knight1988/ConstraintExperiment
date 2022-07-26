5336a0c631f7f610c6cf9cd23331d12125e409cb
Squall Leonhart <Knight1988@gmail.com>
2022-07-26 09:38:37 +0700

    add product category repo

---
+++ Interfaces/Constraint/IProductCategory2Repo.cs
@@ -0,0 +1,7 @@
+﻿using ConstraintExperiment.Models.Constraint;
+
+namespace ConstraintExperiment.Interfaces.Constraint;
+
+public interface IProductCategory2Repo : IBaseRepo<ProductCategory2>
+{
+}
---
+++ Repositories/Constraint/ProductCategory2Repo.cs
@@ -0,0 +1,14 @@
+﻿using ConstraintExperiment.Interfaces.Constraint;
+using ConstraintExperiment.Models.Constraint;
+
+namespace ConstraintExperiment.Repositories.Constraint;
+
+public class ProductCategory2Repo: BaseRepo<ProductCategory2>, IProductCategory2Repo
+{
+    private readonly ConstraintContext _context;
+
+    public ProductCategory2Repo(ConstraintContext context) : base(context, context.ProductCategories, "ProductCategories")
+    {
+        _context = context;
+    }
+}
